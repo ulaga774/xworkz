d084a770e9cb3711c9b31ef41cf447ccbfd6ccd9
ulaga774 <ulaga774.xworkz@gamil.com>
2023-03-01 12:27:53 +0530

    task-1

---
+++ First.java
@@ -0,0 +1,94 @@
+public class First {
+	public static void main(String[] args) {
+		
+		byte marksInTamil = 75;
+		byte batteryPercentage = 15;
+		byte noOfStudents = 45;
+		byte noOfWheelsInBike = 2;
+		byte noOfWheelsInCars = 5;
+		byte noOfDoorsInCars = 7;
+		byte age = 22;
+		byte noOfDaysInMonth = 28;
+		byte noOfDaysInWeek = 7;
+		byte noOfMonthsInYear = 12;
+		
+		short bikeNumber = 2324;
+		short totalMarksInAllSubjects = 417;
+		short noOfDaysInYear = 365;
+		short noOfGearsInBike = 4;
+		short noOfGearsInCar = 5;
+		short thisYear = 2023;
+		short totalNoOfStudentsInCollege = 12573;
+		short noOfStrokEnginInCar = 4;
+		short noOfStrokEnginInBike = 4;
+		short dress = 15;
+		
+		char grade = A;
+		char options = B;
+		char unitForGram = g;
+		char unitForMeter = m;
+		char unitForSecond= S;
+		char unitForKelvin = k;
+		char unitForAmpere = A;
+		char unitForJoule = J;
+		char unitForVolt = v;
+		char unitForNewton = N;
+		
+		boolean asIAmDrinker = true;
+		boolean asYouHaveLaptop = true;
+		boolean asYouAreRich = false;
+		boolean asYouHaveGirlFriend = false;
+		boolean isYouAreFriendNameVarun = true;
+		boolean asYouHaveCar = true;
+		boolean asYouHaveJCB = true;
+		boolean asYouHaveBike = true;
+		boolean asIWillComeFromTamilnadu = true;
+		boolean asYouArePlayboy = false;
+		
+		int moneyInBank = 2298765432;
+		int noOfCarsInShowRoom = 2453265;
+		int noOfBikeInShowRoom = 2172367;
+		int noOfDressInShowRoom = 1224355;
+		int carFinalPrice = 11530000;
+		int bikeFinalPrice = 145000;
+		int oneplus9rtPrice = 47000;
+		int oneplus9rPrice = 49000;
+		int noOfChannelsTamiInSundirect = 180;
+		int rajinikanthTotalMovies = 164;
+		
+		long kamalhaasanTotalMovies = 235;
+		long vijayTotalMovies = 67;
+		long suriyaTotalMovies = 42;
+		long sivaTotalMovies = 15;
+		long jayamraviTotalMovies = 38;
+		long totalPrintMoneyInReserveBank = 9876543210987654321;
+		long samanthaTotalMovies = 54;
+		long nayantharaTotalMovies = 76;
+		long asinTotalMovies = 29;
+		long rambaTotalMovies = 125;
+		
+		float percentageInTamil = 75.76474;
+		float percentageInEnglish = 75.3242;
+		float percentageInMaths = 92.84374;
+		float percentageInScience = 85.8374;
+		float percentageInSocial = 98.4657;
+		float ratioFor1:2 = 0.5;
+		float ratioFor1:3 = 0.3333;
+		float ratioFor1:4 = 0.25;
+		float ratioFor1:15 = 0.0666;
+		float ratioFor1:14 = 0.07142;
+		
+		double ratioFor1:5 = 0.25;
+		double ratioFor1:6 = 0.20;
+		double ratioFor1:7 = 0.14285;
+		double ratioFor1:8 = 0.125;
+		double ratioFor1:9 = 0.11111;
+		double ratioFor1:10 = 0.10;
+		double ratioFor1:11 = 0.0909;
+		double ratioFor1:12 = 0.08333;
+		double ratioFor1:13 = 0.07692;
+		double ratioFor99:100 = 0.99;
+		
+		System.out.println("moneyInBank");
+	} 
+}
