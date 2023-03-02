7273bc04cdb40969bc767b22040ed04ce03e38fa
ulaga774 <ulaga774.xworkz@gamil.com>
2023-03-02 15:18:09 +0530

    task-3 Array

---
+++ Array.java
@@ -0,0 +1,24 @@
+public class Array {
+	public static void main(String[] args) {
+		byte[] marks = {11,12,13,14,15,16,17,18,19,20};
+		short[] bikeNumer = {1122,2233,3344,4455,5566,6677,7788,8899,9900,1010};
+		int[] age = {22,11,33,44,55,66,77,88,99,30};
+		long[] year = {1999,2000,2001,2002,2003,2004,2005,2006,2007,2008};
+		float[] ratio = {1.1f,1.2f,1.3f,1.4f,1.5f,1.6f,1.7f,1.8f,1.9f,2.1f};
+		double[] percentage = {57.0d,57.1d,57.2d,57.3d,57.4d,57.5d,57.6d,57.7d,57.8d,57.9d};
+		char[] units = {'A','B','C','D','E','F','G','H','I','J'};
+		boolean[] yesOrNo = {true,false,true,true,true,false,false,true,true,false};
+		String[] name = {"ulaga","kiran", "varun", "shubes", "tamil" ,"mani","madesh","venkat","thamo","naveen"};
+		
+	
+		System.out.println(marks[0]);
+		System.out.println(bikeNumer[1]);
+		System.out.println(age[2]);
+		System.out.println(year[3]);
+		System.out.println(ratio[4]);
+		System.out.println(percentage[5]);
+		System.out.println(units[6]);
+		System.out.println(yesOrNo[7]);
+		System.out.println(name[8]);
+	}
+}
