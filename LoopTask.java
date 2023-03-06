8d3f66faeec2ec2f5bf35bdab283a566b5250e2c
ulaga774 <ulaga774.xworkz@gamil.com>
2023-03-06 14:45:25 +0530

    LoopTask-4

---
+++ LoopTask.java
@@ -0,0 +1,70 @@
+public class LoopTask {
+	public static void main(String[] args) {
+		byte[] marksInTamil = {75,35,67,37,47};
+		byte[] batteryPercentage = {15,23,45,67,87};
+		byte[] noOfStudents = {45,56,89,2,5};
+		byte[] noOfWheelsInBike = {2,3,4,5};
+		byte[] noOfWheelsInCars = {5,6,7,8,9};
+		byte[] noOfDoorsInCars = {4,5,7,6};
+		byte[] age = {22,34,56,76};
+		byte[] noOfDaysInMonth = {28,14,15,16};
+		byte[] noOfDaysInWeek = {1,2,3,4,7};
+		byte[] noOfMonthsInYear = {1,5,4,3,12};
+		
+		short[] bikeNumber = {2324,7645,3746};
+		short[] totalMarksInAllSubjects = {234,172,384,417};
+		short[] noOfDaysInYear = {198,180,234,365};
+		short[] noOfGearsInBike = {5,6,7,4};
+		short[] noOfGearsInCar = {3,4,6,5};
+		short[] year = {2022,2021,2000,2023};
+		short[] totalNoOfStudentsInCollege = {7843,7328,5465,12573};
+		short[] noOfStrokEnginInCar = {2,4};
+		short[] noOfStrokEnginInBike = {2,4};
+		short[] dress = {43,37,37,15};
+		
+		char[] grade = {'A','a'};
+		char[] options = {'B','b'};
+		char[] unitForGram = {'G','g'};
+		char[] unitForMeter = {'m','M'};
+		char[] unitForSecond= {'S','s'};
+		char[] unitForKelvin = {'k','K'};
+		char[] unitForAmpere = {'A','a'};
+		char[] unitForJoule = {'J','j'};
+		char[] unitForVolt = {'v','v'};
+		char[] unitForNewton = {'N','n'};
+		
+		boolean[] asIAmDrinker = {true,false,true,true,true};
+		boolean[] asYouHaveLaptop = {true,false,true,true,true};
+		boolean[] asYouAreRich = {true,false,true,true,true};
+		boolean[] asYouHaveGirlFriend = {true,false,true,true,true};
+		boolean[] isYouAreFriendNameVarun = {true,false,true,true,true};
+		boolean[] asYouHaveCar = {true,false,true,true,true};
+		boolean[] asYouHaveJCB = {true,false,true,true,true};
+		boolean[] asYouHaveBike = {true,false,true,true,true};
+		boolean[] asIWillComeFromTamilnadu = {true,false,true,true,true};
+		boolean[] asYouArePlayboy = {true,false,true,true,true};
+		
+		int[] moneyInBank = {22987432,3268743,435464,48473};
+		
+		
+		
+		long[] numbers = {1999,2000,2001,2002,2003,2004,2005,2006,2007,2008};
+		
+		
+		float[] ratio = {1.1f,1.2f,1.3f,1.4f,1.5f,1.6f,1.7f,1.8f,1.9f,2.1f};
+		
+		double[] percentage = {57.0d,57.1d,57.2d,57.3d,57.4d,57.5d,57.6d,57.7d,57.8d,57.9d};
+		
+		String[] name = {"ulaga","kiran", "varun", "shubes", "tamil" ,"mani","madesh","venkat","thamo","naveen"};
+		
+		
+		System.out.println(asIAmDrinker[0]);
+		System.out.println(unitForNewton[1]);
+		
+		for (int i=0;i<age.length;i++) {
+			System.out.println(age[i]);
+			
+	
+			}
+	}
+}
