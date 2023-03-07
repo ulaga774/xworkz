fd490f976d505e2eac6be6a91b5b4a8d776a6146
ulaga774 <ulaga774.xworkz@gamil.com>
2023-03-07 16:47:59 +0530

    string and new string task

---
+++ StringClass.java
@@ -0,0 +1,60 @@
+public class StringClass{
+	
+	public static void main(String[] args){
+		
+		String name = "tamil";
+		String animal = "dogs";
+		String movie = "alaipayuthey";
+		String subject = "tamil";
+		String mobile = "oneplus";
+		String bike = "tvs50";
+		String car = "kia";
+		String jcb = "3dx";
+		String passion = "bestDrinker";
+		String brand = "rum";
+		
+		System.out.println(brand);
+		
+		myAttitude();
+			
+		}
+	public static void myAttitude() {
+		String drinker = new String("ulaga");
+		String inspiration = new String("kalamSir");
+		String store = new String("saravana");
+		String mall = new String("lulu");
+		String state = new String("tamilnadu");
+		String country = new String("india");
+		String laptop = new String("dell");
+		String rum = new String("oldMonk");
+		String institute = new String("xworkz");
+		String college = new String("paavai");
+		String school = new String("sathyam");
+		
+		
+		System.out.println(drinker);
+		
+		equal();
+		}
+	public static void equal() {
+		String pet = "dogs";
+		String animals = "dogs";
+		String friend = "dogs";
+		String entertainment = "dogs";
+		String helper = "dogs";
+		
+		System.out.println(pet==entertainment);
+		
+		equalClass();
+	}
+	public static void equalClass() {
+		String drinkers = new String("ulaga");
+		String ffPlayer = new String("ulaga");
+		String smoker = new String("ulaga");
+		String petLover = new String("ulaga");
+		String gamer = new String("ulaga");
+		
+		System.out.println(ffPlayer==gamer);
+	
+	}
+}
