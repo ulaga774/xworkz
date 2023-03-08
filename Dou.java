5814b036108d099a1a1252d63c9a1123c64a05b2
ulaga774 <ulaga774.xworkz@gamil.com>
2023-03-08 08:03:01 +0530

    String and New String

---
+++ Dou.java
@@ -0,0 +1,55 @@
+public class Dou{
+	
+	public static void main(String[] args){
+		
+		String name = "tamil";
+		String animal = "dogs";
+		String movie = "alaipayuthey";
+		String subject = "tamil";
+		String mobile = "oneplus";
+		String bike = "tvs50";
+		String car = "kia";
+		String jcb = "3dx";
+		String passion = "bestDrinker";
+		String brand = "rum";
+		
+		
+	
+		String drinker = new String("ulaga");
+		String inspiration = new String("kalamSir");
+		String store = new String("saravana");
+		String mall = new String("lulu");
+		String state = new String("tamilnadu");
+		String country = new String("india");
+		String laptop = new String("dell");
+		String rum = new String("oldMonk");
+		String institute = new String("xworkz");
+		String college = new String("paavai");
+		String school = new String("sathyam");
+		
+		
+		
+		
+		
+		String pet = "dogs";
+		String animals = "dogs";
+		String friend = "dogs";
+		String entertainment = "dogs";
+		String helper = "dogs";
+		
+		
+		
+		
+		String drinkers = new String("ulaga");
+		String ffPlayer = new String("ulaga");
+		String smoker = new String("ulaga");
+		String petLover = new String("ulaga");
+		String gamer = new String("ulaga");
+		
+		System.out.println(brand);
+		System.out.println(drinker);
+		System.out.println(pet==entertainment);
+		System.out.println(ffPlayer==gamer);
+	
+	}
+}
