28641c30c39d673354a54fdee626fa7df44ca0fa
ulaga774 <ulaga774xworkz@gmail.com>
2023-04-19 21:27:02 +0530

    parameterised

---
+++ ParameterizedConstructor.java
@@ -0,0 +1,12 @@
+public class ParameterizedConstructor{
+		int value;
+		String name;
+	public ParameterizedConstructor(int value,String name){
+	}
+	public static void main(String[] args){
+		ParameterizedConstructor con = new ParameterizedConstructor(23,"kiran");
+		System.out.println(value);
+		System.out.println(name);
+		
+	}
+}
